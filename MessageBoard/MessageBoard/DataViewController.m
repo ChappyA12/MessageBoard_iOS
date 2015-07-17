7c76f387ea900c9e21668e46b0c6b9b70828d670
chappysthebest <chappysthebest1@Chappys-Macbook-Pro-2.local>
2015-07-17 11:59:09 -0400

    Better text saving, editing, and dragging

--- MessageBoard/MessageBoard/DataViewController.m
+++ MessageBoard/MessageBoard/DataViewController.m
@@ -18,6 +18,8 @@
 @property CGPoint lastTouchLocation;
 @property BOOL userKeyboardIsShowing;
 @property TextObject *editedTextObject;
+@property TextObject *translatedTextObject;
+@property CGPoint dragLocationInsideUILabel;
 
 @property NSMutableDictionary <NSNumber *, TextObject *> *UILabelToTextObject;
 @property NSMutableDictionary <NSNumber *, UILabel *> *TextObjectToUILabel;
@@ -30,6 +32,7 @@
     [super viewDidLoad];
     self.dataLabel.text = [NSString stringWithFormat:@"Page: %@", self.pageNumber];
     _userKeyboardIsShowing = NO;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyBoardAppeared:) name:UIKeyboardDidShowNotification object:nil];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -98,7 +101,8 @@
     self.textField = [[UITextField alloc] initWithFrame:CGRectMake(8, 150, self.canvas.frame.size.width-16, 30)];
     if (textObject) { //copy formatting
         self.textField.text = textObject.text;
-        self.textField.font = [UIFont fontWithName:textObject.font size:[textObject.fontSize intValue]];
+        if ([textObject.fontSize floatValue] > 70.0) self.textField.font = [UIFont fontWithName:textObject.font size:70.0];
+        else self.textField.font = [UIFont fontWithName:textObject.font size:[textObject.fontSize intValue]];
         self.textField.textColor = [UIColor colorWithRed:[textObject.color_r intValue] / 255.0 green:[textObject.color_g intValue] / 255.0 blue:[textObject.color_b intValue] / 255.0 alpha:1.0];
     }
     else {
@@ -108,7 +112,15 @@
     self.textField.textAlignment = NSTextAlignmentCenter;
     self.textField.delegate = self;
     self.textField.returnKeyType = UIReturnKeyDone;
+    
     [self.textField becomeFirstResponder];
+    UIVisualEffect *blurEffect;
+    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+    UIVisualEffectView *visualEffectView;
+    visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    visualEffectView.frame = self.view.frame;
+    visualEffectView.tag = 10;
+    [self.view addSubview:visualEffectView];
     [self.view addSubview:self.textField];
     _userKeyboardIsShowing = YES;
 }
@@ -161,6 +173,7 @@
     if (!_editedTextObject) { [self addTextObjectToPageUsingTextField:_textField]; NSLog(@"UILABEL ADDED"); }
     else  { [self updateTextObjectUsingTextField:_textField ]; NSLog(@"UILABEL UPDATED"); }
     [self.textField removeFromSuperview];
+    [[self.view viewWithTag:10] removeFromSuperview];
     _userKeyboardIsShowing = NO;
     _editedTextObject = nil;
 }
@@ -187,22 +200,29 @@
     label.frame = CGRectMake([textObject.location_x floatValue]-textSize.width/2, [textObject.location_y floatValue], textSize.width, textSize.height);
 }
 
+#pragma mark - keyboard notifiction
+
+- (void)keyboardAppeared:(NSNotification*)notification {
+    NSDictionary* keyboardInfo = [notification userInfo];
+    NSValue* keyboardFrameBegin = [keyboardInfo valueForKey:UIKeyboardFrameBeginUserInfoKey];
+    CGRect keyboardFrameBeginRect = [keyboardFrameBegin CGRectValue];
+    self.textField.frame = CGRectMake(8, self.view.frame.size.height-keyboardFrameBeginRect.size.height-80, self.canvas.frame.size.width-16, 80);
+}
+
 #pragma mark - private text movement / scaling / rotation methods
 
-- (void)updateTextObject: (TextObject *) textObject WithLocation: (CGPoint) location {
-    textObject.location_x = [NSNumber numberWithFloat: location.x];
-    textObject.location_y = [NSNumber numberWithFloat: location.y];
+- (void)updateTextObject: (TextObject *) textObject withLocation: (CGPoint) location {
+    textObject.location_x = [NSNumber numberWithFloat: location.x - _dragLocationInsideUILabel.x];
+    textObject.location_y = [NSNumber numberWithFloat: location.y - _dragLocationInsideUILabel.y];
     [self updateUILabelLocationScaleRotationWithTextObject:textObject];
 }
 
 - (void)updateTextObject: (TextObject *) textObject WithScale: (float) scale {
-    NSLog(@"SCALE: %f",scale);
     textObject.scale = [NSNumber numberWithFloat:scale];
     [self updateUILabelLocationScaleRotationWithTextObject:textObject];
 }
 
 - (void)updateTextObject: (TextObject *) textObject WithRotation: (float) rotation {
-    NSLog(@"ROTATE: %f",rotation);
     textObject.rotation = [NSNumber numberWithFloat:rotation];
     [self updateUILabelLocationScaleRotationWithTextObject:textObject];
 }
@@ -211,14 +231,20 @@
     UILabel *label = [_TextObjectToUILabel objectForKey:[NSNumber numberWithUnsignedLong:[textObject hash]]];
     UIFont *font = [UIFont fontWithName:textObject.font size:[textObject.fontSize floatValue]*[textObject.scale floatValue]];
     CGSize textSize = [textObject.text sizeWithAttributes:@{NSFontAttributeName:font}];
-    label.center = CGPointMake([textObject.location_x floatValue], [textObject.location_y floatValue]);
     label.font = font;
-    label.frame = CGRectMake([textObject.location_x floatValue]-textSize.width/2, [textObject.location_y floatValue], textSize.width, textSize.height);
+    label.frame = CGRectMake([textObject.location_x floatValue], [textObject.location_y floatValue], textSize.width, textSize.height);
     label.transform = CGAffineTransformMakeRotation([textObject.rotation floatValue]);
-    [textObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError * __nullable error) {
+    _translatedTextObject = textObject;
+}
+
+- (void)endTranslation {
+    [_translatedTextObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError * __nullable error) {
         if (succeeded) NSLog(@"SAVED");
         else NSLog(@"%@",error);
     }];
+    _translatedTextObject.fontSize = [NSNumber numberWithFloat:[_translatedTextObject.fontSize floatValue]*[_translatedTextObject.scale floatValue]];
+    _translatedTextObject.scale = [NSNumber numberWithFloat:1.0];
+    _translatedTextObject = nil;
 }
 
 #pragma mark - touch recognition
@@ -235,26 +261,30 @@
 #pragma mark - gesture recognition
 
 - (IBAction)userTappedUILabel:(UITapGestureRecognizer *)sender {
-    NSLog(@"TAP");
     TextObject *textEdited = [_UILabelToTextObject objectForKey: [NSNumber numberWithUnsignedLong: [sender.view hash]]];
     _editedTextObject = textEdited;
     [self beginEditingWithTextObject:textEdited];
 }
 
 - (IBAction)userDraggedUILabel:(UIPanGestureRecognizer *)sender {
-    NSLog(@"DRAG");
+    if (sender.state == UIGestureRecognizerStateBegan) _dragLocationInsideUILabel = [sender locationInView:sender.view];
     TextObject *textEdited = [_UILabelToTextObject objectForKey: [NSNumber numberWithUnsignedLong: [sender.view hash]]];
-    [self updateTextObject:textEdited WithLocation:[sender locationInView:_canvas]];
+    [self updateTextObject:textEdited withLocation:[sender locationInView:_canvas]];
+    if (sender.state == UIGestureRecognizerStateEnded) [self endTranslation];
 }
 
 - (IBAction)userScaledUILabel:(UIPinchGestureRecognizer *)sender {
     TextObject *textEdited = [_UILabelToTextObject objectForKey: [NSNumber numberWithUnsignedLong: [sender.view hash]]];
     [self updateTextObject:textEdited WithScale:sender.scale];
+    [self updateTextObject:textEdited withLocation:[sender locationInView:_canvas]];
+    if (sender.state == UIGestureRecognizerStateEnded) [self endTranslation];
 }
 
 - (IBAction)userRotatedUILabel:(UIRotationGestureRecognizer *)sender {
     TextObject *textEdited = [_UILabelToTextObject objectForKey: [NSNumber numberWithUnsignedLong: [sender.view hash]]];
     [self updateTextObject:textEdited WithRotation:sender.rotation];
+    [self updateTextObject:textEdited withLocation:[sender locationInView:_canvas]];
+    if (sender.state == UIGestureRecognizerStateEnded) [self endTranslation];
 }
 
 #pragma mark - text field delegate methods
